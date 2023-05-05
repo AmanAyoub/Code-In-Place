c696c3b2f0648567b9d1f59dd6bc05c5423b28d3
AmanAyoub <130241234+AmanAyoub@users.noreply.github.com>
2023-05-05 10:57:29 +0430

    Update Jigsaw_Karel.py

--- Week 1/Jigsaw_Karel.py
+++ Week 1/Jigsaw_Karel.py
@@ -1,1 +1,70 @@
+from karel.stanfordkarel import *
 
+"""
+Karel should finish the puzzle by picking up the last beeper 
+(puzzle piece) and placing it in the right spot. Karel should 
+end in the same position Karel starts in -- the bottom left 
+corner of the world.
+"""
+
+def main():
+
+  # Move 2 steps forward, pick the beeper and move ahead
+  take_beeper()
+  
+  
+  # Turn to north, move 2 steps up and put the beeper in (3,4)
+  step_up()
+  
+  
+  # Turn around and move 2 steps down
+  step_down()
+  
+  
+  # Face to west, move 3 steps forward and turn to east 
+  move_to_wall()
+  
+
+  
+def take_beeper(): 
+    while no_beepers_present():
+       move()
+    pick_beeper()
+    move()
+    
+   
+def step_up():
+    turn_left()
+    for i in range(2):
+        move()
+    put_beeper()
+   
+   
+def step_down():
+    turn_left()
+    turn_left()
+    while front_is_clear():
+        move()
+
+
+def move_to_wall():
+    turn_right()
+    for i in range(3):
+        move()
+    turn_back()
+    
+    
+    
+def turn_back():
+    for i in range(2):
+        turn_left()
+    
+    
+def turn_right():
+    for i in range(3):
+        turn_left()
+ 
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
