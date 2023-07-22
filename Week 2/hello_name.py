f97d0c65630e505293683f100c2d5c5d315f5a40
AmanAyoub <130241234+AmanAyoub@users.noreply.github.com>
2023-07-22 21:14:34 +0430

    Create hello_name.py

---
+++ Week 2/hello_name.py
@@ -0,0 +1,13 @@
+def main():
+  
+ # Prompt the user for their name and then say hello to them
+
+ prompt = input("What is your name? ")
+ print("Hello " + prompt)
+    
+    
+    
+   
+
+if __name__ == '__main__':
+    main()
